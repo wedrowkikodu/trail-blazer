cb85db839b3e7c67e1d1eea11e8b72d03ae6ffb9
Rafał Boszkowicz <rafal.boszkowicz@gmail.com>
2024-02-21 14:58:26 +0100

    Init commit

---
+++ src/main/java/pl/wedrowkikodu/trailblazer/TrailBlazerApplication.java
@@ -0,0 +1,13 @@
+package pl.wedrowkikodu.trailblazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailBlazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailBlazerApplication.class, args);
+	}
+
+}
